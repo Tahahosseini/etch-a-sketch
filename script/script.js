2de8ebb38f17f003d0712fbf1b0057e9802650f6
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-07-10 01:03:08 +0330

    Make it so when user enters new number the grid changes

--- script/script.js
+++ script/script.js
@@ -13,6 +13,7 @@ for (let i = 0; i <= 15; i++) {
 
 // create 16 squares in each row
 let rows = document.querySelectorAll(".row")
+
 rows.forEach((row) => {
     for (let i = 0; i <= 15; i++) {
         let square = document.createElement("div")
@@ -34,22 +35,82 @@ allSquares.forEach((square) => {
 
 // button
 let btn = document.querySelector(".btn")
+let count = 0
 
-btn.addEventListener("click", function input() {
-
+btn.addEventListener("click", () => {
+    // get an input
     let userInput = prompt("Enter a number between 16 and 100", 16)
 
+    if (userInput < 17 || userInput > 80) {
+        alert("Choose a number between 17 and 80")
+        return;
+    }
+
+    //first change
+    if (count === 0) {
+        ++count;
+        rows.forEach((row) => {
+            canvas.removeChild(row);
+        });
+    }
+
+    //Next changes
+
+    // Remove existing new rows before creating new ones
+    let existingNewRows = document.querySelectorAll(".newrow");
+    existingNewRows.forEach((newRow) => {
+        canvas.removeChild(newRow);
+    });
+
+    // create the new rows
+    for (let i = 0; i < userInput; i++) {
+        let newRow = document.createElement("div")
+        newRow.classList.toggle("newrow")
+        newRow.style.cssText = "display: flex"
+        canvas.appendChild(newRow)
+    }
+
+    // calculate the number of new rows and the new height
+    let newRows = document.querySelectorAll(".newrow")
+    let numberOfRows = newRows.length
+    let newHeight = 100 / numberOfRows
+
+    // change the new row's heights to the new height
+    newRows.forEach((newRow) => {
+        newRow.style.minWidth = "100%"
+        newRow.style.minHeight = `${newHeight}%`
+    })
+
+    // create the new squares
+    newRows.forEach((row) => {
+        for (let i = 0; i < userInput; i++) {
+            let square = document.createElement("div")
+            square.classList.toggle("square")
+            square.style.cssText = "border: solid black 1px; flex: 1"
+            row.appendChild(square)
+        }
+    })
+
+    // mouse hover for the new squares
+    let newSquares = document.querySelectorAll(".square")
+    newSquares.forEach((newSquare => {
+        newSquare.addEventListener("mouseover", () => {
+            newSquare.style.backgroundColor = "black"
+        })
+    }))
+    count++;
 })
 
 // 640px wide
-function changeDimension() {
 
-
-
-
-}
 
 // TO DO (in the function)
 // remove the Original Grid
 // create a for loop that creates n rows based on user input
 // create a for loop that creates n squares in each row based on user input
+
+// To DO
+// create a 0 var
+// each time the button is clicked add 1 to it
+// if the number is 2 or more remove the new childs
+
