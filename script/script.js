8f67de0d6908eb6b56b1f0fefcee05d021a63ef9
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-07-09 21:21:37 +0330

    Add a button to ask user for the new dimension for the canvas
    change the way the squares are sized so they take the whole canvas instead of having a fixed size of 25px so it can be changed easy

--- script/script.js
+++ script/script.js
@@ -1,11 +1,13 @@
 let canvas = document.querySelector(".canvas")
-
+canvas.style.cssText = "width: 640px; height: 640px; margin: 40px auto"
 
 // create 16 rows
 for (let i = 0; i <= 15; i++) {
     let row = document.createElement("div")
     row.classList.toggle("row")
-    row.style.cssText = "display: flex;"
+    row.style.cssText = "display: flex"
+    row.style.minWidth = "100%"
+    row.style.minHeight = "6.25%"
     canvas.appendChild(row)
 }
 
@@ -15,7 +17,7 @@ rows.forEach((row) => {
     for (let i = 0; i <= 15; i++) {
         let square = document.createElement("div")
         square.classList.toggle("square")
-        square.style.cssText = "height : 50px; width : 50px; border: solid black 1px;"
+        square.style.cssText = "border: solid black 1px; flex: 1"
         row.appendChild(square)
     }
 })
@@ -28,3 +30,17 @@ allSquares.forEach((square) => {
         square.style.backgroundColor = "black"
     })
 })
+
+
+// button
+let btn = document.querySelector(".btn")
+
+btn.addEventListener("click", function input() {
+
+    let userInput = prompt("Enter a number between 16 and 100", 16)
+
+})
+
+// 640px wide
+function changeDimension() {
+}
